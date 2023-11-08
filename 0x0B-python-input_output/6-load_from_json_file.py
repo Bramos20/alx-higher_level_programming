cd4ef54c976f3a502e7023b5d2e454624d5ec858
Brian Mosagwe <bramosbrian@gmail.com>
2023-11-07 18:50:16 -0800

    load from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    :param filename: The name of the JSON file to load the object from.
+    :return: The object loaded from the JSON file.
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
