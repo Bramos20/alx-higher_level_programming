21b24cc1f4e479d105edfaceaec7299e4400df59
Brian Mosagwe <bramosbrian@gmail.com>
2023-11-12 03:11:03 -0800

    json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+"""Defines a string-to-JSON function."""
 import json
 
-def to_json_string(my_obj):
-    """
-    Returns the JSON representation of an object as a string.
 
-    :param my_obj: The object to be serialized to JSON.
-    :return: The JSON representation of the object as a string.
-    """
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
     return json.dumps(my_obj)
