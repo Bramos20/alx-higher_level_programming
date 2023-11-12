eb0d8a82c8b647aebb3e90f9dcb1c7666cda0e91
Brian Mosagwe <bramosbrian@gmail.com>
2023-11-12 03:14:54 -0800

    from json string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
 import json
 
-def from_json_string(my_str):
-    """
-    Returns a Python data structure represented by a JSON string.
 
-    :param my_str: The JSON string to be parsed.
-    :return: The Python data structure represented by the JSON string.
-    """
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
